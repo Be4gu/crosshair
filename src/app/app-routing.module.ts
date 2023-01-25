3827dbb6241adff2ad3fa57b7728b166f25d5836
Be4gu <ioncristicristea@gmail.com>
2023-01-25 19:42:50 +0100

    Url de settings funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ProSettingsComponent } from './components/pro-settings/pro-settings.com
 
 const routes: Routes = [{ path: 'miras', component: MirasComponent },
 { path: '', redirectTo: "/miras", pathMatch: 'full' },
-{ path: 'settings', component: ProSettingsComponent }];
+{ path: 'settings', component: ProSettingsComponent },
+{ path: 'settings/:id', component: ProSettingsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/pro-settings/pro-settings.component.ts
+++ src/app/components/pro-settings/pro-settings.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pro-settings',
   templateUrl: './pro-settings.component.html',
   styleUrls: ['./pro-settings.component.css']
 })
-export class ProSettingsComponent {
-  players =["Mixwell","TenZ","Yay", "Cned"]
+export class ProSettingsComponent implements OnInit {
+  infoPlayer=[{pro:'mixwell', info: 'Info correspondiente a mixwell'},{pro:'tenz', info: 'Info correspondiente a tenz'}]
+  players =["mixwell","tenz","yay", "cned"]
+  active!: string;
+  constructor(private activeR: ActivatedRoute, private route: Router){
+    this.active= 'mixwell'
+  }
+  ngOnInit(){
+    this.activeR.queryParams.subscribe((params) => {
+      this.active = params['player'];
+    })
+  }
+  activeButton(name: string) {
+    this.active = name.toLowerCase();
+    this.setQueryParams();
+  }
+  setQueryParams() {
+    this.route.navigate(['/settings'], {
+      queryParams: { player: this.active },
+    })
+  }
 }
