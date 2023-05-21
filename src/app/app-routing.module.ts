9895aeda76dffe2bfaacc602c80d28b70efc7747
Cristi <you@example.com>
2023-05-21 21:06:02 +0200

    Se ha implementado el SSR 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { MirasComponent } from './components/miras/miras.component';
 import { ProSettingsComponent } from './components/pro-settings/pro-settings.component';
 
 const routes: Routes = [
-  { path: 'miras', component: MirasComponent },
   { path: '', redirectTo: '/miras', pathMatch: 'full' },
+  { path: 'miras', component: MirasComponent },
   { path: 'settings', component: ProSettingsComponent },
   // { path: 'settings?player=:id', component: ProSettingsComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
